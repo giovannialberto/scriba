32b3a681c4764caa51389a388ab7e208e8e1e21d
Giovanni Alberto Falcione <giovannialbertofalcione@gmail.com>
2026-03-30 15:14:40 +0200

    chore: bump version to 0.23.0 (#76)

--- Formula/scriba.rb
+++ Formula/scriba.rb
@@ -1,13 +1,13 @@
 class Scriba < Formula
   desc "Modern CLI tool for recording and transcribing audio using OpenAI Whisper"
   homepage "https://github.com/giovannialberto/scriba"
-  version "0.22.3"
+  version "0.23.0"
   
   if Hardware::CPU.intel?
-    url "https://github.com/giovannialberto/scriba/releases/download/v0.22.3/scriba-x86_64-apple-darwin"
+    url "https://github.com/giovannialberto/scriba/releases/download/v0.23.0/scriba-x86_64-apple-darwin"
     sha256 "db5f1ba162208f23b96d742e8dbca898fe00361d35b16a9260dc085364a08602"
   else
-    url "https://github.com/giovannialberto/scriba/releases/download/v0.22.3/scriba-aarch64-apple-darwin"
+    url "https://github.com/giovannialberto/scriba/releases/download/v0.23.0/scriba-aarch64-apple-darwin"
     sha256 "a9651093536682ad3069d884987e0395c07dd0c8746bd9697312fd00db77b3d6"
   end
   
